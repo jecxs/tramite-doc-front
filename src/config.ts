f9c1c2e149ca4f58d916aad0b3644887f93ed701
Marcos Alanya <asesoralanya19@gmail.com>
2025-12-06 17:14:27 -0500

    feat: correct env

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,12 @@
-import env from 'env-var';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+const SOCKET_URL_EXPLICIT = process.env.NEXT_PUBLIC_SOCKET_URL || 'ws://localhost:3000';
+const NODE_ENV = (process.env.NODE_ENV || 'development') as 'development' | 'test' | 'production';
 
-const API_URL = env.get('NEXT_PUBLIC_API_URL').default('http://localhost:3000/api').asUrlString();
-const SOCKET_URL_EXPLICIT = env.get('NEXT_PUBLIC_SOCKET_URL').default('ws://localhost:3000').asUrlString();
-const NODE_ENV = env.get('NODE_ENV').default('development').asEnum(['development', 'test', 'production']);
+console.log('API_URL', API_URL);
+console.log('SOCKET_URL_EXPLICIT', SOCKET_URL_EXPLICIT);
 
 export const config = {
-  API_URL: API_URL,
+  API_URL,
   SOCKET_URL: SOCKET_URL_EXPLICIT || new URL(API_URL).origin,
   NEXT_PUBLIC_API_URL: API_URL,
   NODE_ENV,
